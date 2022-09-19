2e4ecf337070f14f6de191c6e846e5be01b6f735
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-09-20 00:51:43 +0200

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Siyabonga's Portfolio</title>
+      </Head>
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
